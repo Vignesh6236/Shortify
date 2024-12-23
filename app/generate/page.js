c648bf485e2c16fced9a78ca8c2a086a1b5a3331
Vignesh N <vigneshrage@gmail.com>
2024-12-23 14:32:18 +0530

    saving data to mongodb, dynamic routes, redirecting to url

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,12 +1,40 @@
 "use client";
 
+import Link from "next/link";
 import React, { useState } from "react";
 
 const generate = () => {
     const [url, seturl] = useState("");
     const [shortURL, setShortURL] = useState("");
-    const [generated, setGenerated] = useState(false);
+    const [generated, setGenerated] = useState("");
 
+    const generateUrl = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "url": url,
+            "shortUrl": shortURL
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("/api/generate", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result)
+                alert(result.message)
+                seturl("")
+                setShortURL("")
+                setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shortURL}`)
+            })
+            .catch((error) => console.error(error));
+    }
 
 
     return (
@@ -30,9 +58,17 @@ const generate = () => {
                 />
                 <div className="bg-gradient-to-r from-teal-100 to-pink-200 w-full">
 
-                    <button className=" glass5 w-full rounded-md py-1">Generate</button>
+                    <button
+                        onClick={generateUrl}
+                        className=" glass5 w-full rounded-md py-1">Generate</button>
                 </div>
             </div>
+            {generated && <>
+                <span className="font-bold text-lg">Your Links</span>
+                <code>
+                    <Link target="_blank" href={generated}>{generated}</Link>
+                </code>
+            </>}
         </div>
     );
 };
